5461da070d854895485a521e9edb606d20eb4426
Mark <mce777@gmail.com>
2018-06-11 21:59:19 +0200

    up to 5

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store →</button>
+        </form>
+      </Fragment>
+    );
+  }
+}
+
+export default StorePicker;
+// also, dont forget to import the css in the index file that is already prepard
