23bbce73186e5fa760f57f035fb98d9368adf38c
Mark <mce777@gmail.com>
2018-07-16 23:11:56 +0200

    state 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,20 +1,34 @@
-import React from "react";
-import Header from "./Header";
-import Inventory from "./Inventory";
-import Order from "./Order";
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div className="catch-of-the-day">
-          <div className="menu">
-            <Header tagline="Fresh Seafood" />
-          </div>
-          <Order />
-          <Inventory />
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood" />
         </div>
-      )
-    }
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
 }
 
 export default App;
