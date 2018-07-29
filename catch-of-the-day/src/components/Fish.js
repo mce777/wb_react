6541c80d1c076d38a36240747a5a14068ac01e3f
Mark <mce777@gmail.com>
2018-07-29 21:24:58 +0200

    through 16

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
+  handleClick = () => {
+    // notice 'index'
+    // Since you can't pass down 'key' from the app component, gotta do it like this
+    this.props.addToOrder(this.props.index);
+  };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +19,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add To Cart' : 'Sold out!'}
+        </button>
       </li>
     );
   }
